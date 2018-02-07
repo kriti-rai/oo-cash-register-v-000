e19e46f285b6ed257922f11e6ca597d3ac1fb653
Kriti Rai <kriti.rai@gmail.com>
2018-02-07 19:53:29 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,71 +1,32 @@
-# 
-# class CashRegister
-#   attr_accessor :total, :discount, :price, :qty
-# 
-# 
-#   def initialize(total = 0, discount = 20)
-#    @total = total
-# 
-#    @discount = discount
-#  end
-# 
-#  def add_item(item, price, qty = 1)
-#    @price = price
-#    @qty = qty
-#    @total += @price * @qty
-#  end
-# 
-#  def apply_discount
-#    puts @discount
-#    if @discount != 0
-#      discount = (@discount.to_f/100)
-#     total_discount = (@total * discount)
-#     @total = @total - total_discount
-#     "After the discount, the total comes to #{@total}."
-#    else
-#      "There is no discount to apply."
-# 
-#     # return "After the discount, the total comes to #{@total}."
-#    end
-#  end
-# 
-# end
+
 class CashRegister
+  attr_accessor :total, :discount, :price, :qty
 
-  attr_accessor :items, :discount, :total, :last_transaction
 
-  def initialize(discount=0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
+  def initialize(discount = 0)
+   @total = 0
 
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
-      items << title
-    end
-    self.last_transaction = amount * quantity
-  end
+   @discount = discount
+ end
 
-  def apply_discount
-    if discount != 0
-      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
-      "After the discount, the total comes to $#{self.total}."
-    else
-      "There is no discount to apply."
-    end
-  end
+ def add_item(item, price, qty = 1)
+   @price = price
+   @qty = qty
+   @total += @price * @qty
+ end
 
-  def void_last_transaction
-    self.total = self.total - self.last_transaction
-  end
+ def apply_discount
+   puts @discount
+   if @discount != 0
+     discount = (@discount.to_f/100)
+    total_discount = (@total * discount)
+    @total = @total - total_discount
+    "After the discount, the total comes to #{@total}."
+   else
+     "There is no discount to apply."
+
+    # return "After the discount, the total comes to #{@total}."
+   end
+ end
+
 end
-
-
-
-
-
-
-
-
