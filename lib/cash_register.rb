0b46d991996e3c72eccf1b700bf5f1120bbd70b9
Kriti Rai <kriti.rai@gmail.com>
2018-02-07 20:15:06 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
  end
 
  def add_item(item, price, qty = 1)
-   @total += price * qty
+   self.total += price * qty
    qty.times {@items << item}
    @last_transaction = price * qty
  end
